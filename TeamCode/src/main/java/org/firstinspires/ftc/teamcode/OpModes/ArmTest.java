7f473098739191b32260b37ba94dbfde4cd9bb51
Kabir <kabirgoyal@icloud.com>
2024-01-06 15:18:30 -0500

    asdf
    asdf

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArmTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.wrappers.ArmWrapper;
+import org.firstinspires.ftc.teamcode.wrappers.JoystickWrapper;
+
+@TeleOp
+public class ArmTest extends LinearOpMode {
+    ArmWrapper armWrapper;
+    JoystickWrapper joystickWrapper;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        joystickWrapper = new JoystickWrapper(gamepad1, gamepad2);
+        armWrapper = new ArmWrapper(hardwareMap, telemetry, armPidConstants.Kp, armPidConstants.Ki, armPidConstants.Kd, armPidConstants.maxIntegralSum, armPidConstants.a);
+
+        waitForStart();
+        while(!isStopRequested()) {
+            armWrapper.PPArmMove(joystickWrapper);
+        }
+
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PIDController.java
@@ -7,8 +7,8 @@ public class PIDController {
     private double Kp; // Proportional gain
     private double Ki; // Integral gain
     private double Kd; // Derivative gain
-    private double maxIntegralSum;
-    private double a;
+    //private double maxIntegralSum;
+    //private double a;
     private double prevError;
     private double integral;
 
@@ -20,8 +20,8 @@ public class PIDController {
         this.Kp = Kp;
         this.Ki = Ki;
         this.Kd = Kd;
-        this.maxIntegralSum = maxIntegralSum;
-        this.a = a;
+        //this.maxIntegralSum = maxIntegralSum;
+        //this.a = a;
         this.prevError = 0.0;
         this.integral = 0.0;
     }
@@ -30,17 +30,17 @@ public class PIDController {
         double error = setpoint - current;
         integral += error*dt.seconds();
         errorChange = error - prevError;
-        currentFilterEstimate = (a * prevFilterEstimate) + (1-a) * errorChange;
+        //currentFilterEstimate = (a * prevFilterEstimate) + (1-a) * errorChange;
         prevFilterEstimate = currentFilterEstimate;
 
         double derivative = currentFilterEstimate / dt.seconds();
 
         // Limit the integral term to prevent windup
-        if (integral > maxIntegralSum) {
+       /* if (integral > maxIntegralSum) {
             integral = maxIntegralSum;
         } else if (integral < -maxIntegralSum) {
             integral = -maxIntegralSum;
-        }
+        }*/
 
         double output = (Kp * error) + (Ki * integral) + (Kd * derivative);
 
@@ -52,17 +52,17 @@ public class PIDController {
     public double calculatewitherror(double error, ElapsedTime dt) {
         integral += error*dt.seconds();
         errorChange = error - prevError;
-        currentFilterEstimate = (a * prevFilterEstimate) + (1-a) * errorChange;
+        //currentFilterEstimate = (a * prevFilterEstimate) + (1-a) * errorChange;
         prevFilterEstimate = currentFilterEstimate;
 
         double derivative = currentFilterEstimate / dt.seconds();
 
         // Limit the integral term to prevent windup
-        if (integral > maxIntegralSum) {
+        /*if (integral > maxIntegralSum) {
             integral = maxIntegralSum;
         } else if (integral < -maxIntegralSum) {
             integral = -maxIntegralSum;
-        }
+        }*/
 
         double output = (Kp * error) + (Ki * integral) + (Kd * derivative);
 
