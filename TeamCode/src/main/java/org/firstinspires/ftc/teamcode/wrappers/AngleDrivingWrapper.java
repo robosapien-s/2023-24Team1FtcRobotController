ea4e282a4f44e157a953e3513552dfe9a17a9354
Kabir <kabirgoyal@icloud.com>
2024-01-15 15:31:35 -0500

    a
    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/AngleDrivingWrapper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/wrappers/AngleDrivingWrapper.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.util.AngleThing;
 import org.firstinspires.ftc.teamcode.util.PIDController;
 import org.firstinspires.ftc.teamcode.util.RevIMUv2;
 
@@ -18,18 +19,20 @@ public class AngleDrivingWrapper{
 
     HardwareMap hardwareMap;
     Telemetry telemetry;
-
+    AngleThing angleThing;
     DcMotorEx motorFrontLeft;
     DcMotorEx motorFrontRight;
 
     DcMotorEx motorBackLeft;
     DcMotorEx motorBackRight;
 
-    PIDController anglePID;
 
-    public AngleDrivingWrapper(HardwareMap inHardwareMap, Telemetry inTelemetry, double Kp, double Ki, double Kd, double maxIntegralSum, double a) {
+    public AngleDrivingWrapper(HardwareMap inHardwareMap, Telemetry inTelemetry) {
+        angleThing = new AngleThing();
+
         hardwareMap = inHardwareMap;
         telemetry = inTelemetry;
+        angleThing.init(hardwareMap, telemetry);
         //0
         motorFrontLeft = (DcMotorEx) hardwareMap.dcMotor.get("fL");
         //1
@@ -44,7 +47,6 @@ public class AngleDrivingWrapper{
         motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        anglePID = new PIDController(Kp, Ki, Kd, maxIntegralSum, a);
 
         //motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE); //setting the right side motors to reverse so they go the right directiond
         //motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -85,10 +87,8 @@ public class AngleDrivingWrapper{
         double y = -joystickWrapper.gamepad1GetLeftStickY(); // Remember, this is reversed! | Defining the y variable
         double x = joystickWrapper.gamepad1GetLeftStickX() * 1.1; // Counteract imperfect strafing | Defining the x variable
         //double t = joystickWrapper.gamepad1GetRightStickX() * rotSpeed; // Defining the rx (right x) variable
+        double t = angleThing.getRange();
 
-        double angleCorrection = anglePID.calculatewitherror(angleError, dt);
-
-        double t = -angle + angleCorrection; // how to make this into a -1 to 1 power
 
 
         double xr = (x * Math.cos(angle)) - (y * Math.sin(angle)); //x with rotation in account
