84459c52262428cb1a8cdc5db56978be1ac411b9
williams104 <67794864+williams104@users.noreply.github.com>
2024-01-15 17:57:45 -0500

    lets go!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/IMUReader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/IMUReader.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,6 +12,7 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.opencv.core.Mat;
 
 @TeleOp
 public class IMUReader extends LinearOpMode {
@@ -42,13 +45,15 @@ public class IMUReader extends LinearOpMode {
         backRightMotor = hardwareMap.dcMotor.get("bR");
 
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
         RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
 
         // Now initialize the IMU with this mounting orientation
         // This sample expects the IMU to be in a REV Hub and named "imu".
         imu = hardwareMap.get(IMU.class, "imu");
         imu.initialize(new IMU.Parameters(orientationOnRobot));
+
+        imu.resetYaw();
         waitForStart();
         while (!isStopRequested()) {
 
@@ -58,12 +63,13 @@ public class IMUReader extends LinearOpMode {
             roll = orientation.getRoll(AngleUnit.DEGREES);
             yaw = orientation.getYaw(AngleUnit.DEGREES);
 
-            if (length(gamepad1.left_stick_x, gamepad1.left_stick_y) > .5) {
-                targetHeading = Math.toDegrees(Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x));
+            if (length(gamepad1.right_stick_x, gamepad1.right_stick_y) > .5) {
+                targetHeading = Math.toDegrees(Math.atan2(gamepad1.right_stick_y, -gamepad1.right_stick_x)) + 90;
             }  // Save for telemetry
 
             // Determine the heading current error
             double headingError = targetHeading - yaw;
+
 
             // Normalize the error to be within +/- 180 degrees
             while (headingError > 180) headingError -= 360;
@@ -76,8 +82,15 @@ public class IMUReader extends LinearOpMode {
             telemetry.addData("Yaw", yaw);
             telemetry.addData("Error", headingError);
             telemetry.addData("Range",Range.clip(headingError * PGain, -1, 1));
+            telemetry.addData("correction angle",yaw);
 
-            MoveMecanum( 0,0,Range.clip(headingError * PGain, -1, 1));
+
+            Translation2d translation2d =RotateAngle(gamepad1.left_stick_x,gamepad1.left_stick_y,yaw);
+
+
+
+
+            MoveMecanum( translation2d.getX(),translation2d.getY(),Range.clip(headingError * PGain, -1, 1));
 
             telemetry.update();
 
@@ -96,5 +109,13 @@ public class IMUReader extends LinearOpMode {
         backLeftMotor.setPower(y - x + rx);
         frontRightMotor.setPower(y - x - rx);
         backRightMotor.setPower(y + x - rx);
+    }
+
+    Translation2d RotateAngle(double x,double y,double angle){
+        double rAngle = Math.toRadians(angle);
+
+        double rotatedX = x*Math.cos(rAngle) - y*Math.sin(rAngle);
+        double rotatedY = x*Math.sin(rAngle) + y*Math.cos(rAngle);
+        return new Translation2d(rotatedX,rotatedY);
     }
 }
