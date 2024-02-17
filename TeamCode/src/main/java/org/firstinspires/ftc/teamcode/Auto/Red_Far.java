18fb81201bfffb177190d63f2709423bf71fe738
williams104 <67794864+williams104@users.noreply.github.com>
2024-02-17 11:16:26 -0500

    Update Red_Far.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Red_Far.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Red_Far.java
@@ -11,6 +11,7 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 public class Red_Far extends LinearOpMode {
     Pose2d startPose = new Pose2d(-36,-62, Math.toRadians(-90));
 
+    static int barcodeInt = 3;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,25 +19,81 @@ public class Red_Far extends LinearOpMode {
 
         drive.setPoseEstimate(startPose);
 
-        TrajectorySequence trajectory1 = drive.trajectorySequenceBuilder(startPose)
-                .lineToLinearHeading(new Pose2d(-36, -36, Math.toRadians(-90)))
-                .waitSeconds(.5)
-                .lineToLinearHeading(new Pose2d(-60, -36, Math.toRadians(0)))
-                .waitSeconds(.5)
-                .lineToLinearHeading(new Pose2d(51,-36, Math.toRadians(0)))
-                .waitSeconds(.5)
-                .lineToLinearHeading(new Pose2d(-60,-36, Math.toRadians(0)))
-                .waitSeconds(.5)
-                .lineToLinearHeading(new Pose2d(51,-36, Math.toRadians(0)))
-                .waitSeconds(.5)
-                .lineToLinearHeading(new Pose2d(48,-62, Math.toRadians(0)))
-                .lineToLinearHeading(new Pose2d(60,-62, Math.toRadians(0)))
-                .build();
+        TrajectorySequence trajectory1 = drive.trajectorySequenceBuilder(new Pose2d(-36, -62, Math.toRadians(-90)))
+            .setReversed(true)
+            .splineTo(new Vector2d(-40, -36), Math.toRadians(135))
+            .setReversed(false)
+            .waitSeconds(.5)
+            .strafeLeft(15)
+            .setReversed(true)
+            .splineTo(new Vector2d(-60,-12), Math.toRadians(180))
+            .setReversed(false)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(0))
+            .splineTo(new Vector2d(51, -30), Math.toRadians(0))
+            .setReversed(true)
+            .waitSeconds(.5)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(180))
+            .splineTo(new Vector2d(-60, -12), Math.toRadians(180))
+            .waitSeconds(.5)
+            .setReversed(false)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(0))
+            .splineTo(new Vector2d(51, -30), Math.toRadians(0))
+            .lineToLinearHeading(new Pose2d(48,-12, Math.toRadians(0)))
+            .lineToLinearHeading(new Pose2d(60,-12, Math.toRadians(0)))
+            .build();
+        TrajectorySequence trajectory2 = drive.trajectorySequenceBuilder(new Pose2d(-36, -62, Math.toRadians(-90)))
+            .lineToLinearHeading(new Pose2d(-36, -32, Math.toRadians(-90)))
+            .waitSeconds(.5)
+            .lineToLinearHeading(new Pose2d(-50, -32, Math.toRadians(-90)))
+            .setReversed(true)
+            .splineTo(new Vector2d(-60,-12), Math.toRadians(180))
+            .setReversed(false)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(0))
+            .splineTo(new Vector2d(51, -36), Math.toRadians(0))
+            .setReversed(true)
+            .waitSeconds(.5)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(180))
+            .splineTo(new Vector2d(-60, -12), Math.toRadians(180))
+            .waitSeconds(.5)
+            .setReversed(false)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(0))
+            .splineTo(new Vector2d(51, -30), Math.toRadians(0))
+            .lineToLinearHeading(new Pose2d(48,-12, Math.toRadians(0)))
+            .lineToLinearHeading(new Pose2d(60,-12, Math.toRadians(0)))
+            .build();
+        TrajectorySequence trajectory3 = drive.trajectorySequenceBuilder(new Pose2d(-36, -62, Math.toRadians(-90)))
+            .setReversed(true)
+            .splineTo(new Vector2d(-32, -36), Math.toRadians(45))
+            .setReversed(false)
+            .waitSeconds(.5)
+            .strafeRight(10)
+            .setReversed(true)
+            .splineTo(new Vector2d(-60,-12), Math.toRadians(180))
+            .setReversed(false)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(0))
+            .splineTo(new Vector2d(51, -42), Math.toRadians(0))
+            .setReversed(true)
+            .waitSeconds(.5)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(180))
+            .splineTo(new Vector2d(-60, -12), Math.toRadians(180))
+            .waitSeconds(.5)
+            .setReversed(false)
+            .splineTo(new Vector2d(24, -12), Math.toRadians(0))
+            .splineTo(new Vector2d(51, -30), Math.toRadians(0))
+            .lineToLinearHeading(new Pose2d(48,-12, Math.toRadians(0)))
+            .lineToLinearHeading(new Pose2d(60,-12, Math.toRadians(0)))
+            .build();
 
         waitForStart();
 
         if (!isStopRequested()) {
-            drive.followTrajectorySequence(trajectory1);
+            if(barcodeInt == 1){
+                drive.followTrajectorySequence(trajectory1);
+            } else if (barcodeInt == 2) {
+                drive.followTrajectorySequence(trajectory2);
+            }else {
+                drive.followTrajectorySequence(trajectory3);
+            }
         }
 
     }
